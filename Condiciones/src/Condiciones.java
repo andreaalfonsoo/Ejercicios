15392186a6fcd9260fe708d63f03abd45b852589
andreaalfonsoo <andreaalfonsoo@gmail.com>
2015-05-27 12:47:06 +0200

    Condiciones

---
+++ Condiciones/src/Condiciones.java
@@ -0,0 +1,29 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+public class Condiciones extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Condiciones frame = new Condiciones();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
