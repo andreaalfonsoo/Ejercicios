c1c565726c891df7cc1d4c47a4a5fdbf046d1975
andreaalfonsoo <andreaalfonsoo@gmail.com>
2015-05-27 12:50:02 +0200

    Imprimir Por Pantalla

---
+++ ImprimirPorPantalla/src/ImprimirPorPantalla.java
@@ -0,0 +1,10 @@
+
+public class ImprimirPorPantalla {
+
+	public static void main(String[] args) {;
+		int i=5;
+		boolean fr = false;
+		System.out.println("El valor es:"+fr+".Y el valor de i es:"+i);
+	}
+
+}
